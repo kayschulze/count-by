67be2bd6154093ea93a5eb483e29997d2b94f1cf
Kim Schulze <kimberlykayschulze@gmail.com>
2017-07-11 09:30:37 -0700

    change countUpNumber to countUpToNumber

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
+// function does the work of counting and displaying to a certain number.
+// variable i acts as an index that gets displayed as the counting number.
+var countingUpDisplay = function(by, upto) {
+  for (i = by, i < upto, i += by) {
+    
+  }
+};
+
 $(function() {
   $("count-up-form").submit(function(event) {
     var countByNumber = $("countBy").val();
-    var countUpNumber = $("countUp").val();
+    var countUpToNumber = $("countUp").val();
+
+    countingUpDisplay(countByNumber, countUpToNumber);
   });
 });
