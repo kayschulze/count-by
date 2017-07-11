0d268a2886be7a46900027798f0f7afc18639956
Kim Schulze <kimberlykayschulze@gmail.com>
2017-07-11 09:48:56 -0700

    performs submit method when button is pressed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 // function does the work of counting and displaying to a certain number.
 // variable i acts as an index that gets displayed as the counting number.
 var countingUpDisplay = function(by, upto) {
-  for (i = by, i < upto, i += by) {
+  for (i = by; i < upto; i += by) {
     $("p").append(" " + i + " ");
+    $("#ourCount").show();
   }
 };
 
 $(function() {
-  $("count-up-form").submit(function(event) {
-    var countByNumber = $("countBy").val();
-    var countUpToNumber = $("countUp").val();
+  $("#count-up-form").submit(function(event) {
+    event.preventDefault();
+    var countByNumber = $("#countBy").val();
+    var countUpToNumber = $("#countUp").val();
 
+    console.log(countByNumber, countUpToNumber);
     countingUpDisplay(countByNumber, countUpToNumber);
   });
 });
