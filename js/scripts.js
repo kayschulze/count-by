c10f4f28accdaa6fe77d6b0f88eee24b909c7d78
Kim Schulze <kimberlykayschulze@gmail.com>
2017-07-11 10:31:04 -0700

    includes number validation alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 // function does the work of counting and displaying to a certain number.
 // variable i acts as an index that gets displayed as the counting number.
 var countingUpDisplay = function(by, upto) {
+  $("#count").text("");
   $("#ourCount").show();
   for (var i = by; i <= upto; i += by) {
-    $("p").append(" " + i + " ");
-    alert(i);
+    $("p").append("   " + i + "   ");
+  }
+};
+
+var numberValidation = function(number) {
+  if (isNaN(number)) {
+    alert("This is not a number.  Try again.")
+  }
+  else if (number == 0) {
+    alert("I love zeros!  But that doesn't help us here.")
+  }
+  else if (number < 0) {
+    alert("Let's try to be positive here.")
   }
 };
 
@@ -14,7 +26,9 @@ $(function() {
     var countByNumber = parseInt($("#countBy").val());
     var countUpToNumber = parseInt($("#countUp").val());
 
-    console.log(countByNumber, countUpToNumber);
+    numberValidation(countByNumber);
+    numberValidation(countUpToNumber);
+
     countingUpDisplay(countByNumber, countUpToNumber);
   });
 });
